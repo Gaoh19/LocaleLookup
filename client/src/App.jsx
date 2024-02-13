3da28ca235757548e12d0abe3a328e4d37e798d2
Gaoh19 <aburahman918@gmail.com>
2024-02-13 11:28:47 +0530

    Config RouterDOM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 const App = () => {
-    return  <div>Abdur's Application</div>;
+    return  <div>
+        <Router>
+            <Switch>
+                <Route exact path='/' Component={Home}/>
+                <Route exact path='/location/:locationid' Component={locationDetail}/>
+                <Route exact path='/location/:locationid/update' Component={updateLocation}/>
+            </Switch>
+        </Router>
+    </div>;
 };
 
 export default App;
